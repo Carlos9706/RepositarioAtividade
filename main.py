20fd40537a7b92afc45ee3a28f6a0b08e9075e3c
Carlos9706 <ca97pc@gmail.com>
2026-04-09 12:01:44 -0300

    Código inicial da calculadora
    Esse é o código inicial da calculadora, versão 0.0.1

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Calculadora")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print("A soma é: {}".format(soma))
+print("A subtração é: {}".format(subtracao))
+print("A multiplicação é: {}".format(multiplicacao))
+print("A divisão é: {}".format(divisao))
