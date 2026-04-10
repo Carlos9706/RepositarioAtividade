096e84e389574954a860ecfba4c12ac3d4b561be
Carlos9706 <ca97pc@gmail.com>
2026-04-10 11:16:47 -0300

    Atualizacao-Calculadora
    Atualização para a calculadora acrescentando uma repetição ao menu, possibilitando não apenas calcular varias vezes mais também sair quando quiser.

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 print("Calculadora")
 print("Digite um número válido dentre as opções de menu: ")
 
-print("--- Menu da Calculadora ---")
-print("1 - Soma")
-print("2 - subtracao")
-print("3 - multiplicacao")
-print("4 - divisao")
+while True:
+    print("--- Menu da Calculadora ---")
+    print("1 - Soma")
+    print("2 - subtracao")
+    print("3 - multiplicacao")
+    print("4 - divisao")
+    print("5 - sair")
 
-opcoes = int(input("Digite uma opção do menu: "))
+    opcoes = int(input("Digite uma opção do menu: "))
 
-numero1 = int(input("Digite o primeiro número: "))
-numero2 = int(input("Digite o segundo número: "))
+    if opcoes == 5:
+        print("Saindo da calculadora.....")
+        break
 
-if opcoes == 1:
-    resultado = numero1 + numero2
-    print("A soma é: {}".format(resultado))
+    numero1 = int(input("Digite o primeiro número: "))
+    numero2 = int(input("Digite o segundo número: "))
 
-elif opcoes == 2:
-    resultado = numero1 - numero2
-    print("A subtração é: {}".format(resultado))
+    if opcoes == 1:
+        resultado = numero1 + numero2
+        print("A soma é: {}".format(resultado))
 
-elif opcoes == 3:
-    resultado = numero1 * numero2
-    print("A multiplicação é: {}".format(resultado))
+    elif opcoes == 2:
+        resultado = numero1 - numero2
+        print("A subtração é: {}".format(resultado))
 
-elif opcoes == 4:
-    resultado = numero1 / numero2
-    print("A divisão é: {}".format(resultado))
+    elif opcoes == 3:
+        resultado = numero1 * numero2
+        print("A multiplicação é: {}".format(resultado))
 
-else:
-    print("Opção invalida!")
+    elif opcoes == 4:
+        resultado = numero1 / numero2
+        print("A divisão é: {}".format(resultado))
+
+    else:
+        print("Opção invalida!")
